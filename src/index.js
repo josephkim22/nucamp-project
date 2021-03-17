43fd91f198fe751a495c9f32434afffae34840f1
Joe Kim <jkjundo@yahoo.com>
2021-03-17 14:12:25 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+import App from './App';
 
 
 ReactDOM.render(
