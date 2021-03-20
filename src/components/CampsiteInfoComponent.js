cfcceaed1117d73326a079daa6a9357cbd18849b
Joe Kim <jkjundo@yahoo.com>
2021-03-20 12:58:40 -0700

    week4 task2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
 
 function RenderCampsite({ campsite }) {
     return (
@@ -20,15 +21,19 @@ function RenderComments({ comments }) {
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-                {comments.map(comment => {
-                    return (
-                        <div key={comment.id}>
-                            <p>{comment.text}</p>
-                            <p>{comment.author}</p>
-                            <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                        </div>);
-                })
+                {
+                    comments.map(comment => {
+                        return (
+                            <div key={comment.id}>
+                                <p>
+                                    {comment.text}<br />
+                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                                </p>
+                            </div>
+                        );
+                    })
                 }
+                <CommentForm />
             </div>
         );
     }
@@ -36,11 +41,70 @@ function RenderComments({ comments }) {
 }
 
 class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+
     render() {
         return (
-        <Button outline>
-            <i className="fa fa-pencil fa-lg" />Submit Comment
-        </Button>
+            <div>
+                <Button outline onClick={this.toggleModal}>
+                    <i className="fa fa-pencil fa-lg" /> Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select model=".rating" id="rating" name="rating"
+                                    className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author" id="author" name="author"
+                                    placeholder="Your Name"
+                                    className="form-control"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text">Comment</Label>
+                                <Control.textarea model=".text" id="text" name="text"
+                                    rows="6"
+                                    className="form-control"
+                                />
+                            </div>
+                            <Button type="submit" color="primary">
+                                Submit
+                            </Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
         );
     }
 }
@@ -58,17 +122,15 @@ function CampsiteInfo(props) {
                         <h2>{props.campsite.name}</h2>
                         <hr />
                     </div>
-            </div>
+                </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments} />
-                    <CommentForm/>
                 </div>
             </div>
         );
     }
-    return  <div />;
+    return <div />;
 }
-
 
 export default CampsiteInfo;
